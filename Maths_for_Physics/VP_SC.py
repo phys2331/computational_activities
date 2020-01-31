f146509965ed1cc2e3284d4bebeec9159718180c
drlmnop <51173067+drlmnop@users.noreply.github.com>
2020-01-31 08:11:32 -0700

    Add Vector Problems notebook and py script

---
+++ Maths_for_Physics/VP_SC.py
@@ -0,0 +1,29 @@
+class Vectors():
+
+    def q1():
+        #Question: <1, 2, 3> + <2, 3, 4>.
+
+        ans1 = input("answer: ")
+        if ans1 == "<3,5,7>" or ans1 == "<3, 5, 7>":
+            print("\nGreat! Now try it with different numbers.")
+        else:
+            print("\nNot quite. Remember to add each component separately. Rerun the cell to try again.")
+
+
+    def q2():
+        #Question: <-5, 2, 0> + <-15, -2, 0>.
+
+        ans1 = input("answer: ")
+        if ans1 == "<-20,0,0>" or ans1 == "<-20, 0, 0>":
+            print("\nGreat! Now try it with different numbers.")
+        else:
+            print("\nNot quite. Remember to add each component separately. Rerun the cell to try again.")
+
+    def q3():
+        #Question: <-5, 2, 0> + <-15, -2, 0>.
+
+        ans1 = input("answer: ")
+        if ans1 == "<0.015, 5.025, 6.076> m" or ans1 == "<0.015,5.025,6.076> m":
+            print("\nWonderful!")
+        else:
+            print("\nNot quite. Remember to convert the units. Rerun the cell to try again.")
