985c934e184209e616a7858b84af18897432437f
drlmnop <51173067+drlmnop@users.noreply.github.com>
2020-02-12 08:09:05 -0700

    Add more VPs - dot, cross, unit

--- Maths_for_Physics/VP_SC.py
+++ Maths_for_Physics/VP_SC.py
@@ -90,3 +90,111 @@ class Vectors():
             print("\nAmazing!")
         else:
             print("\nNot quite. Rerun the cell to try again.")
+
+    def q11():
+        #Question: 3 * <8, 2, 1>.
+
+        ans1 = input("answer: ")
+        if ans1 == "<24, 6, 3> m/s^2" or ans1 == "<24,6,3> m/s^2":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q12():
+        #Question: |<5, 2, 1>| N/C.
+
+        ans1 = input("answer: ")
+        if ans1 == "5.477 N/C":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q13():
+        #Question: |<0, 5, 5>|.
+
+        ans1 = input("answer: ")
+        if ans1 == "7.071":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q14():
+        #Question: |<3, 2, 4>|.
+
+        ans1 = input("answer: ")
+        if ans1 == "5.385 V/m":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q15():
+        #Question: rhat of <7, 6, 5>. 49, 36, 25 10.4881
+
+        ans1 = input("answer: ")
+        if ans1 == "<0.667, 0.572, 0.477>" or ans1 == "<0.667, 0.572, 0.477>":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q16():
+        #Question: -6 * <3, 4, 5>
+
+        ans1 = input("answer: ")
+        if ans1 == "<-18, -24, -30>" or ans1 == "<-18,-24,-30>":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q17():
+        #Question: <1, 6, -2> dot <2, -1, 3>
+
+        ans1 = input("answer: ")
+        if ans1 == "-10":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q18():
+        #Question: <2, 2, -1> dot <1, -6, 2>
+
+        ans1 = input("answer: ")
+        if ans1 == "-12":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q19():
+        #Question: 6 * 10 * cos(30)
+
+        ans1 = input("answer: ")
+        if ans1 == "51.962":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q20():
+        #Question: <2, 4, 6> cross <3, 2, 5>
+
+        ans1 = input("answer: ")
+        if ans1 == "<8, 8, -8>" or ans1 == "<8,8,-8>":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q21():
+        #Question: <-1, -5, 6> cross <4, -5, 9>
+
+        ans1 = input("answer: ")
+        if ans1 == "<-15, 33, 25>" or ans1 == "<-15,33,25>":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
+
+    def q21():
+        #Question: <4, -5, 9> cross <-1, -5, 6>
+
+        ans1 = input("answer: ")
+        if ans1 == "<15, -33, -25>" or ans1 == "<15,-33,-25>":
+            print("\nAmazing!")
+        else:
+            print("\nNot quite. Rerun the cell to try again.")
